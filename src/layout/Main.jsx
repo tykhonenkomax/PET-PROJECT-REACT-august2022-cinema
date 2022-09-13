9c412574bb0a6cb7cf80752d31e0e1834f8a2678
Max <tykhonenko.work@gmail.com>
2022-09-13 17:58:34 +0300

    build2

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -14,18 +14,24 @@ class Main extends Component {
 
   componentDidMount() {
     this.setState({loading: true});
-    fetch(`http://www.omdbapi.com/?apikey=1d8278e2&s=matrix`)
+    fetch(`https://www.omdbapi.com/?apikey=1d8278e2&s=matrix`)
         .then(response => response.json())
         .then(data => this.setState({movies: data.Search, loading: false}))
-  }
+  }.catch((err)=>{
+    console.error(err)
+  this.setState({loading: false});
+  })
 
 
   searchMovies = (str,type='all') => {
     this.setState({loading: true});
-    fetch(`http://www.omdbapi.com/?apikey=${API_KEY}&s=${str}${type !== 'all' ? `&type=${type}` : ''}`)
+    fetch(`https://www.omdbapi.com/?apikey=${API_KEY}&s=${str}${type !== 'all' ? `&type=${type}` : ''}`)
         .then(response => response.json())
         .then(data => this.setState({movies: data.Search, loading: false}))
-  }
+  }.catch((err)=>{
+  console.error(err)
+  this.setState({loading: false});
+})
 
   render() {
     const {movies, loading} = this.state;
